2e1dfacee11dfb541fd30af77cdeeca2bd814f84
Jake Lehmer <lehmjac2@isu.edu>
2016-03-16 17:40:43 +0000

    basic movement and drawing functionality in experimental application

--- Experimental_Programs/dnc_AnsiMovement.c
+++ Experimental_Programs/dnc_AnsiMovement.c
@@ -7,9 +7,97 @@
 //JPL 3/14/16
 int main(int argc, char ** argv)
 {
+signal(SIGINT,control_c_catch);
 struct termios new_t,old_t;
 disable_echo(&new_t,&old_t);
-
+     
+     pthread_t timer_thread;
+     pthread_t graphics_thread;
+     pthread_t ui_thread;
+     
+     uint8_t tps = 10;
+     
+     object * items = malloc(sizeof(object)*6);
+     
+     initialize_items(items);
+     
+     graphics_threadinfo info_graphics =
+     {
+     .checked = 1,
+     .player_index = 0,
+     .size = 6,
+     .in_game_objects = items
+     };
+     
+     user_controls info_controls =
+     {
+     .checked = 1,
+     .command = ' '
+     };
+     
+     if(pthread_create(&timer_thread,NULL,game_timer,(void*)&tps ) <0)
+     {
+     perror("Thread Creation Failure");
+     return 1;
+     }
+     
+     if(pthread_create(&graphics_thread,NULL,graphics_handler,(void*)&info_graphics) < 0)
+     {
+     perror("Thread Creation Failure");
+     return 1;
+     }
+     
+     if(pthread_create(&ui_thread,NULL,handle_user_controls,(void*)&info_controls) < 0)
+     {
+     perror("Thread Creation Failure");
+     return 1;
+     }
+     
+     while(killall != -1)
+     {
+          if(info_controls.checked != 1)
+          {
+               switch(info_controls.command)
+               {
+               case 'w' :
+                    {
+                    items[0].y_pos -=1;
+                    items[0].direction = NORTH;
+                    info_graphics.checked = 1;
+                    break;
+                    }
+               case 'a' :    
+                     {
+                    items[0].x_pos -=1;
+                    items[0].direction = WEST;
+                    info_graphics.checked = 1;
+                    break;
+                    }
+               case 's' :    
+                     {
+                    items[0].y_pos +=1;
+                    items[0].direction = SOUTH;
+                    info_graphics.checked = 1;
+                    break;
+                    }
+               case 'd' :    
+                     {
+                    items[0].x_pos +=1;
+                    items[0].direction = NORTH;
+                    info_graphics.checked = 1;
+                    break;
+                    }
+               case 'W' :break;
+               case 'A' :break;
+               case 'S' :break;
+               case 'D' :break;
+               }
+               info_controls.checked = 1;
+          }
+          
+     usleep(1000);
+     }
+     free(items);
 
 reenable_echo(&old_t);
 }
@@ -42,7 +130,73 @@ void reenable_echo(struct termios * old_t)
 
 //=========-------------
 //JPL 3/14/16 This is the graphics thread for the system
+void * graphics_handler(void * _thread_info)
+{
+     struct winsize w;
+     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+     graphics_threadinfo * _info = (graphics_threadinfo * ) _thread_info;
+     unsigned int scrn_col = w.ws_col;
+     unsigned int scrn_row = w.ws_row;
+     long int frames = 0;
+     int sync_check = game_sync;
+     int ref_x = _info->in_game_objects[_info->player_index].x_pos;
+     int ref_y = _info->in_game_objects[_info->player_index].y_pos;
+     printf("%d,%d\n",ref_x,ref_y);
+     while(killall != -1)
+          {
+          if(sync_check != game_sync)
+               {
+               if(_info->checked != 0)
+                    {
+                    ref_x = _info->in_game_objects[_info->player_index].x_pos;
+                    ref_y = _info->in_game_objects[_info->player_index].y_pos;
+                    
+                    draw_screen(_info,ref_x,ref_y,scrn_col,scrn_row);
+                    _info->checked = 0;
+                    sync_check = game_sync;
+                    }
+               }
+          else
+               {
+               usleep(1000);
+               }
+          }
 
+}
+//=========-------------
+//JPL 3/16/16 this will draw the screen around the player
+void draw_screen(graphics_threadinfo * _info, int ref_x, int ref_y, unsigned int col, unsigned int row)
+{
+int rel_x = 0;
+int rel_y = 0;
+printf("\x1b[2J");
+     for(int i =0; i < _info->size; i++)
+     {
+     rel_x = (ref_x - _info->in_game_objects[i].x_pos);
+     rel_y = (ref_y - _info->in_game_objects[i].y_pos);
+     if((rel_x*rel_x + rel_y*rel_y) <= 1000)
+          {
+          printf("\x1b[%d;%dH%s\x1b[1;1H",( row/2 - rel_y),(col/2 - rel_x*2),get_object(&(_info->in_game_objects[i])));
+          }
+     }
+     
+     fflush(stdout);
+
+}
+
+//JPL 3/16/16 - This will return the ansi string corresponding to type and color with all cursor movements being relative
+char * get_object(object * obj)
+{
+     char * returnString = malloc(sizeof(char)*100);
+     switch(obj->type)
+     {
+     case PLAYER : sprintf(returnString,"\x1b[%dm    \x1b[B\x1b[4D    \x1b[A\x1b[m",obj->color); //TODO: Handle the directionality of the obj
+     case BLOCK : sprintf(returnString,"\x1b[%dm    \x1b[B\x1b[4D    \x1b[A\x1b[m",obj->color);
+     }
+     return returnString;
+}
+
+//=========-------------
 //=========-------------
 
 //JPL 3/14/16 This is the timer system thread it will use usleep to time the system as it gives adequate precision for a system, 
@@ -54,7 +208,87 @@ unsigned int wait_time = 1000000/tps;
 
 while(killall != 1)
      {
-     sync *= -1;
+     game_sync *= -1;
      usleep(wait_time);
      }
 }
+//=========-------------
+//JPL 3/7/16 This is the control c catch so that it can end gracefully
+void control_c_catch(int catch)
+{
+killall = -1;
+}
+
+//=========-------------
+//JPL 3/16/16 this is the initialization method
+void initialize_items(object * _items)
+{
+_items[0] = (object)
+     {
+     .type = PLAYER,
+     .color = GREEN,
+     .direction = NORTH,
+     .x_pos = 3,
+     .y_pos = 0
+     };
+     
+_items[1] = (object)
+     {
+     .type = BLOCK,
+     .color = BLUE,
+     .x_pos = 10,
+     .y_pos = 0
+     };
+     
+_items[2] = (object)
+     {
+     .type = BLOCK,
+     .color = BLUE,
+     .x_pos = -10,
+     .y_pos = 0
+     };
+     
+_items[3] = (object)
+     {
+     .type = BLOCK,
+     .color = BLUE,
+     .x_pos = 0,
+     .y_pos = 10
+     };
+     
+_items[4] = (object)
+     {
+     .type = BLOCK,
+     .color = BLUE,
+     .x_pos = 0,
+     .y_pos = -10
+     };
+_items[5] = (object)
+     {
+     .type = BLOCK,
+     .color = BLUE,
+     .x_pos = 0,
+     .y_pos = 0
+     };
+
+}
+
+//=========-------------
+//JPL 3/16/16 - This is the thread handler for the user controls
+void * handle_user_controls(void * _thread_info)
+{
+user_controls * control_info = (user_controls *) _thread_info;
+while(killall != -1)
+     {
+     control_info->command = getchar();
+     control_info->checked = 0;
+     }
+}
+
+
+
+
+
+
+
+
--- Experimental_Programs/dnc_AnsiMovement.h
+++ Experimental_Programs/dnc_AnsiMovement.h
@@ -14,9 +14,12 @@
 #ifndef ansi_movementc
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <termios.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
 
 
 //--------------------------
@@ -25,22 +28,32 @@
 #define PLAYER 0
 #define BLOCK 1
 
-#define RED 0
-#define GREEN 1
-#define BLUE 2
+#define RED 41
+#define GREEN 42
+#define BLUE 44
+
+#define NORTH 0
+#define EAST 1
+#define SOUTH 2
+#define WEST 3
 typedef struct
 {
 uint8_t type;  //0-player 1-block
-uint8_t color; //0-Red 1-Green 2-Blue
+uint8_t color; //41-Red 42-Green 44-Blue
+uint8_t direction; //0-North 1-East 2-South 3-West
 int x_pos;
 int y_pos;
 }object;
+
+//JPL 3/16/16 - this is responible for buiilding the array of objects that will be passed around
+//NOTE: A way needs to be found in order to create a way to build the environment, currently this is completely ad_hoc
+void initialize_items(object * _items);
 
 
 //--------------------------
 //Control Mutexes and corresponding variables
 int killall = 0;
-int sync = -1; //This should only ever be changed by the timer method, otherwise it will be the value that is waited on
+int game_sync = -1; //This should only ever be changed by the timer method, otherwise it will be the value that is waited on
 
 
 //--------------------------
@@ -57,21 +70,42 @@ void reenable_echo(struct termios * old_t);
 //Each of the threads have their info stuct underneath it that needs to be passed in
 
 //JPL 3/14/16 This is the control thread for reading user input
+//TODO: do not use the stdin, use the actual device instead
 void * handle_user_controls(void * _thread_info);
+typedef struct
+{
+char command;
+int checked; //same as for the graphics info
+}user_controls;
+
+
+//--------------------------
+//Graphics
+//--------------------------
 
 //JPL 3/14/16 This is the drawing thread for the console
 void * graphics_handler(void * _thread_info);
 typedef struct
 {
-int checked; //0 for assign new 1 for new has been assigned new graphics thread waits on this value before calculating next frame in buffer, and then waits on sync before drawing 
+int checked; //0 for assign new 1 for new has been assigned new graphics thread waits on this value before calculating next frame in buffer, and then waits on sync before drawing
+int player_index; //This ives the index for the current user's player so that the screen can be drawn to them 
+int size;
 object * in_game_objects;
-}graphics_threadinfo
+}graphics_threadinfo;
+
+//JPL 3/16/16 - This is the method respionsible for drawing the screen around the player
+void draw_screen(graphics_threadinfo * _info,int ref_x, int ref_y, unsigned int col, unsigned int row);
+
+//JPL 3/16/16 - This will return the ansi string corresponding to type and color with all cursor movements being relative
+char * get_object(object * obj);
+
 
 //JPL 3/14/16 This is the timer system for the entire game it will be using
 //Pass this an uint8 corresponding to frames per second
 void * game_timer(void * _ticks_per_second);
 
-
+//JPL 3/16/16 This is the control c catch so that it may end properly
+void control_c_catch(int catch);
 
 //--------------------------
 
